3e8589941e51bf445cfcba8fbf2d52615214384c
vessellook <vessellook@gmail.com>
2022-12-27 15:38:18 +0300

    Add DateTimePretty component and its dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import React, { useState } from 'react';
+import moment from 'moment';
+import shortid from 'shortid';
 
 function DateTime(props) {
   return <p className="date">{props.date}</p>;
+}
+
+function chooseSuffix(number, suffices) {
+  if (number >= 11 && number <= 14) {
+    return suffices[2]
+  }
+  const c = number % 10;
+  if (c === 1) {
+    return suffices[0];
+  }
+  if (c >= 2 && c <= 4) {
+    return suffices[1];
+  }
+  return suffices[2];
+}
+
+function DateTimePretty(props) {
+  let prettyDate = props.date;
+  const minutes = moment().diff(props.date, 'minutes');
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(minutes / 60 / 24);
+  if (days > 0) {
+    prettyDate = `${days} ${chooseSuffix(days, ['день', 'дня', 'дней'])} назад`;
+  } else if (hours > 0) {
+    prettyDate = `${hours} ${chooseSuffix(hours, ['час', 'часа', 'часов'])} назад`;
+  } else if (minutes > 0) {
+    prettyDate = `${minutes} ${chooseSuffix(minutes, ['минута', 'минуты', 'минут'])} назад`;
+  }
+  return <DateTime {...props} date={prettyDate} />;
 }
 
 function Video(props) {
@@ -13,13 +44,13 @@ function Video(props) {
         allow="autoplay; encrypted-media"
         allowfullscreen
       ></iframe>
-      <DateTime date={props.date} />
+      <DateTimePretty date={props.date} />
     </div>
   );
 }
 
 function VideoList(props) {
-  return props.list.map((item) => <Video url={item.url} date={item.date} />);
+  return props.list.map((item) => <Video key={item.id} url={item.url} date={item.date} />);
 }
 
 export default function App() {
@@ -42,13 +73,13 @@ export default function App() {
     },
     {
       url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
-      date: '2018-01-01 16:17:00',
+      date: moment().subtract(15, 'hour'),
     },
     {
       url: 'https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0',
-      date: '2017-12-02 05:24:00',
+      date: moment().subtract(12, 'minute'),
     },
-  ]);
+  ].map((obj) => ({...obj, id: shortid()})));
 
   return <VideoList list={list} />;
 }
